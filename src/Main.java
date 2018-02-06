dd057715c6e1b7aaeda15319130d4ae0aee8c0ec
Sharkeee <sharkmen1996@gmail.com>
2018-02-06 18:09:39 +0100

    arraychallenge

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        int[] myIntArray = new int[10];
 
-        for (int i = 0; i < myIntArray.length; i++) {
-            myIntArray[i] = i * 10;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+        for (int i = 0; i < myIntegers.length; i++) {
+            System.out.println("Element [" + i + "] typed values was " + myIntegers[i]);
         }
-        printArray(myIntArray);
+
+        System.out.println("Srednia: " + getAverage(myIntegers));
     }
 
-    public static void printArray(int[] array) {
-        for(int i =0; i<array.length; i++) {
-            System.out.println("Eleemtn [" + i + "] value is: " + array[i]);
+
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " intege values,\r");
+        int[] values = new int[number];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = scanner.nextInt();
         }
+
+        return values;
+    }
+
+    public static double getAverage(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        double sumaElementow = sum / array.length;
+
+        return sumaElementow;
     }
 }
